cf35103e2c2c3470b182dfd8ca94cba511d836c7
Venkat <venkatkilari5@gmail.com>
2024-09-13 20:33:58 -0700

    intial commit

--- Code.java
+++ Code.java
@@ -8,9 +8,18 @@ public class Data{
              //bug 1282
      }
 
-public int m3(){
+   public int m3(){
              //bug 1289
      }
+
+   public int m4(){
+            //bug 1999
+     }
+
+   public String m5(){
+              //bug-1234
+
+   }
 
 
 }
